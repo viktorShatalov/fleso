47c9d029cf24f77fd32caaa6a1e1c357e396a990
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2021-05-18 18:50:31 +0300

    ++

--- dist/js/main.js
+++ dist/js/main.js
@@ -124,14 +124,32 @@ jQuery(document).ready(function ($) {
     $(".header__mobile-menu").toggleClass("active");
     $("body").toggleClass("overflow");
   });
+  var prev = "<button type=\"button\" class=\"slick-prev\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"29.448\" height=\"14.474\" viewBox=\"0 0 29.448 14.474\"> <path id=\"long-arrow-alt-right\" d=\"M8.812,14.867H28.659a.789.789,0,0,1,.789.789v3.681a.789.789,0,0,1-.789.789H8.812v3.028a1.578,1.578,0,0,1-2.693,1.115L.462,18.613a1.577,1.577,0,0,1,0-2.231l5.657-5.657a1.578,1.578,0,0,1,2.693,1.115Z\" transform=\"translate(0 -10.26)\" fill=\"#ebe9e0\"/> </svg></button>";
+  var next = "<button type=\"button\" class=\"slick-next\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"29.448\" height=\"14.474\" viewBox=\"0 0 29.448 14.474\"> <path id=\"long-arrow-alt-right\" d=\"M20.636,14.867H.789A.789.789,0,0,0,0,15.656v3.681a.789.789,0,0,0,.789.789H20.636v3.028a1.578,1.578,0,0,0,2.693,1.115l5.657-5.657a1.577,1.577,0,0,0,0-2.231l-5.657-5.657a1.578,1.578,0,0,0-2.693,1.115Z\" transform=\"translate(0 -10.26)\" fill=\"#ebe9e0\"/> </svg></button>";
   $(".slider-js").slick({
     slidesToShow: 3,
     slidesToScroll: 2,
     dots: false,
     infinite: false,
     arrows: true,
-    prevArrow: "<button type=\"button\" class=\"slick-prev\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"29.448\" height=\"14.474\" viewBox=\"0 0 29.448 14.474\">\n        <path id=\"long-arrow-alt-right\" d=\"M8.812,14.867H28.659a.789.789,0,0,1,.789.789v3.681a.789.789,0,0,1-.789.789H8.812v3.028a1.578,1.578,0,0,1-2.693,1.115L.462,18.613a1.577,1.577,0,0,1,0-2.231l5.657-5.657a1.578,1.578,0,0,1,2.693,1.115Z\" transform=\"translate(0 -10.26)\" fill=\"#ebe9e0\"/>\n      </svg></button>",
-    nextArrow: "<button type=\"button\" class=\"slick-next\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"29.448\" height=\"14.474\" viewBox=\"0 0 29.448 14.474\">\n      <path id=\"long-arrow-alt-right\" d=\"M20.636,14.867H.789A.789.789,0,0,0,0,15.656v3.681a.789.789,0,0,0,.789.789H20.636v3.028a1.578,1.578,0,0,0,2.693,1.115l5.657-5.657a1.577,1.577,0,0,0,0-2.231l-5.657-5.657a1.578,1.578,0,0,0-2.693,1.115Z\" transform=\"translate(0 -10.26)\" fill=\"#ebe9e0\"/>\n    </svg></button>",
+    prevArrow: prev,
+    nextArrow: next,
+    responsive: [{
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }]
+  });
+  $(".related .product__items").slick({
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    dots: false,
+    arrows: false,
+    infinite: false,
+    prevArrow: prev,
+    nextArrow: next,
     responsive: [{
       breakpoint: 767,
       settings: {
@@ -190,7 +208,31 @@ jQuery(document).ready(function ($) {
   $("ul.sub-category.js-el").attr("data-content", "").on("click", function () {
     $(this).children("li").toggle("show");
     $(this).toggleClass("active");
-  });
+  }); // woof settings
+
+  var priceStr = jQuery(".price_label");
+
+  if (priceStr.length) {
+    split();
+  }
+
+  function split() {
+    var priceHtml = priceStr.html().trim().split("").splice(7).join("");
+    var newHtml = priceStr.html(priceHtml);
+    return newHtml;
+  }
+
+  $("button.woof_reset_search_form").text("Zurücksetzen"); // header menu slicky   
+
+  $(document).on('scroll', function () {
+    var height = $(window).scrollTop();
+
+    if (height > 300 - 78) {
+      $(".header__bottom").addClass("fixed");
+    } else {
+      $(".header__bottom").removeClass("fixed");
+    }
+  }); // header__menu
 });
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "jquery")))
 
